fc222297f790fdf1d1ed3d586f0846eaf386c92b
Ümit Ünver <umitunver9@gmail.com>
2024-04-24 15:55:49 +0300

    team area be

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -6,17 +6,33 @@ import Container from '@/src/components/XContainer';
 import XHeader from '@/src/composite/XHeader';
 import XFooter from '@/src/composite/XFooter';
 
+import { nextFetcher } from '@/src/helpers/fetcherHelper';
 import Layouts from '@/src/layouts';
 
-export default function TeamPage() {
+interface IProps {
+  data: {
+    image: undefined | string;
+    spot_text: string;
+    title: string;
+  };
+}
+
+TeamPage.getInitialProps = async () => {
+  const data = await nextFetcher(
+    `${process.env.NEXT_PUBLIC_API_URL + '/items/bs_team_area'}`
+  );
+  return { data: data.data[0] };
+};
+
+export default function TeamPage({ data }: IProps) {
   return (
     <Layouts>
       <XHeader color="light" />
       <main>
         <XPageTitle
-          bgImage={'/assets/bannerImage2.jpg'}
-          title="Proje Geliştirme Ekibimiz"
-          marqueTitle="Ekibimiz"
+          marqueTitle={data?.title}
+          title={data?.spot_text}
+          bgImage={data?.image}
           bgColor="white"
         />
         <section className={styles.wrapper}>
